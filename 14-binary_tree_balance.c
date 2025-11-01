5789d67b7308322dca93379e9896985ff54cc1b7
Afnan9999 <afnan-911@outlook.sa>
2025-11-01 20:25:49 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,13 +2,14 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: Pointer to the root node of the tree
  *
  * Return: Height or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+    size_t left_height;
+    size_t right_height;
 
     if (tree == NULL)
         return (0);
@@ -16,12 +17,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
 
-    return ((left_height > right_height ? left_height : right_height) + 1);
+    if (left_height > right_height)
+        return (left_height + 1);
+    return (right_height + 1);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
+ * @tree: Pointer to the root node of the tree
  *
  * Return: Balance factor or 0 if tree is NULL
  */
